12e3d06915be7e8cf083302f69058536fe9ef60d
Harvey Ji <harvey.ji123@gmail.com>
2024-09-11 21:34:45 -0500

    Updated Nav Bar to include social icons and connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import gradPhoto from './assets/img/graduationphotocropped.jpg';
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <NavBar/>
+    <div className='main-content'>
       <div>
         <img src={gradPhoto}/>
       </div>
       <h1>Coming Soon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          My Excitement: {count}
-        </button>
-      </div>
-
+    </div>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,31 +2,57 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 import './NavBar.css';
+import { useState, useEffect } from 'react';
+import { SiIndeed, SiLinkedin, SiInstagram } from 'react-icons/si';
 
 function NavBar() {
+  const [activeLink, setActiveLink] = useState('');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll =() => {
+      if (window.scrolly > 50){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return() => window.removeEventListener("scroll", onScroll);
+  })
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">Harvey's Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About Me</Nav.Link>
-            <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Coming Soon</NavDropdown.Item>
-              {/* <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-            </NavDropdown>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#aboutme" className={activeLink === 'aboutme' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('aboutme')}>About Me</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
+          <span className='navbar-text'>
+            <div className="social-icons">
+              <a href="https://profile.indeed.com/?hl=en_US&co=US&from=gnav-homepage"><SiIndeed/></a>
+              <a href="https://www.linkedin.com/in/harvey-ji-69a630200/"><SiLinkedin/></a>
+              <a href="https://www.instagram.com/harv.g/?next=%2F&hl=en"><SiInstagram/></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
