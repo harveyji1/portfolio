8106191b699590c4d7ef1133550719b4604f3311
Harvey Ji <harvey.ji123@gmail.com>
2024-11-12 21:10:45 -0600

    Projects Pictures Added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,10 @@
 import './Projects.css';
+import darkMoon from "../../assets/img/ADarkMoon.jpg";
+import cookItSaved from "../../assets/img/CookItSaved.jpg";
+import cookItHome from "../../assets/img/CookItHome.jpg";
+import cookItPost from "../../assets/img/CookItPost.jpg";
+import jinoSki from "../../assets/img/JinoSkiResort.jpg";
+
 
 function Projects(){
     return(
@@ -8,31 +14,47 @@ function Projects(){
         </div>
         <div className="projectsBodyContainer">
             <div className="projectCard">
+                <div>
+                    <img src={cookItHome} className='projectImg'/>
+                    <img src={cookItSaved} className='projectImg'/>
+                    <img src={cookItPost} className='projectImg'/>
+                    
+                </div>
                 <h2 className="projectTitle">Cook-It</h2>
                 <div className="projectContent">
+
                     <article>
                         <div>
-                            <h4>React/React Native</h4>
+                            <h4>React Native</h4>
                         </div>
                     </article>
                 </div>
             </div>
             <div className="projectCard">
+                    <div>
+                        <img src={jinoSki} className='projectImg'/>
+                    </div>
+                <h2 className="projectCard">Jino Ski Resort</h2>
+                <div className="projectContent">
+
+                    
+                    <article>
+                        <div>
+                            <h4>PHP</h4>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div className="projectCard">
+                <div>
+                    <img src={darkMoon} className='projectImg'/>
+                </div>
                 <h2 className="projectCard">A Dark Moon</h2>
                 <div className="projectContent">
+                    
                     <article>
                         <div>
-                            <h4>Cypress</h4>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div className="projectCard">
-                <h2 className="projectCard">My Portfolio</h2>
-                <div className="projectContent">
-                    <article>
-                        <div>
-                            <h4>Cypress</h4>
+                            <h4>JavaScript</h4>
                         </div>
                     </article>
                 </div>
