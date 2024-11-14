3b87566e397c2de3066f81664b8565bda38b329d
Harvey Ji <harvey.ji123@gmail.com>
2024-11-13 20:36:49 -0600

    Projects formatted and button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,9 +4,18 @@ import cookItSaved from "../../assets/img/CookItSaved.jpg";
 import cookItHome from "../../assets/img/CookItHome.jpg";
 import cookItPost from "../../assets/img/CookItPost.jpg";
 import jinoSki from "../../assets/img/JinoSkiResort.jpg";
-
+import Button from 'react-bootstrap/Button';
 
 function Projects(){
+    const cookItButton = () => {
+        window.location.href = 'https://github.com/harveyji1/KUCapstone23';
+    };
+    const skiResortButton = () => {
+    window.location.href = 'https://github.com/harveyji1/EECS447FinalProject';
+    };
+    const darkMoonButton = () => {
+    window.location.href = 'https://github.com/harveyji1/A-Dark-Moon';
+    };
     return(
     <div id="projects">
         <div className="projectsHeaderContainer">
@@ -18,45 +27,29 @@ function Projects(){
                     <img src={cookItHome} className='projectImg'/>
                     <img src={cookItSaved} className='projectImg'/>
                     <img src={cookItPost} className='projectImg'/>
-                    
                 </div>
                 <h2 className="projectTitle">Cook-It</h2>
                 <div className="projectContent">
-
-                    <article>
-                        <div>
-                            <h4>React Native</h4>
-                        </div>
-                    </article>
+                    <h4 className='projectInfo'>React Native, Expo, TypeScript</h4>
+                    <Button className='projectLink' onClick={cookItButton}>GitHub</Button>
                 </div>
             </div>
             <div className="projectCard">
-                    <div>
-                        <img src={jinoSki} className='projectImg'/>
-                    </div>
-                <h2 className="projectCard">Jino Ski Resort</h2>
+                    <img src={jinoSki} className='projectImg'/>
+                <h2 className="projectTitle">Jino Ski Resort</h2>
                 <div className="projectContent">
-
-                    
-                    <article>
-                        <div>
-                            <h4>PHP</h4>
-                        </div>
-                    </article>
+                    <h4 className='projectInfo'>PHP, HTML, SQL, CSS</h4>
+                    <Button className='projectLink' onClick={skiResortButton}>GitHub</Button>
                 </div>
             </div>
             <div className="projectCard">
                 <div>
                     <img src={darkMoon} className='projectImg'/>
                 </div>
-                <h2 className="projectCard">A Dark Moon</h2>
-                <div className="projectContent">
-                    
-                    <article>
-                        <div>
-                            <h4>JavaScript</h4>
-                        </div>
-                    </article>
+                <h2 className="projectTitle">A Dark Moon</h2>
+                <div className="projectContent">                    
+                    <h4 className='projectInfo' onClick={darkMoonButton}>JavaScript, HTML, Github</h4>
+                    <Button className='projectLink'>GitHub</Button>
                 </div>
             </div>
         </div>
