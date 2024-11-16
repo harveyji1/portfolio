eb09cdbe0e2b84db50b173e115588982390b3fc2
Harvey Ji <88002662+harveyji1@users.noreply.github.com>
2024-11-16 11:17:54 -0600

    Dark Mode toggle, resume, palette

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,8 @@ import './Home.css';
 function Home(){
     return(
     <section id="home">
-        <h5 className='helloHeader'>Hello, I am</h5>
-        <h1>Harvey Ji</h1>
+        {/* <h5 >Hello, I am</h5> */}
+        <h1 className='helloHeader'>Harvey Ji</h1>
         <h6>Software QA Engineer and Developer</h6>
         <div className="gradphotoContainer">
             <img src={gradPhoto} className='gradphoto'/>
