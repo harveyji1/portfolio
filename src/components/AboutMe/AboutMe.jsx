5493b843061ea211eedb452f033e8653250e3892
Harvey Ji <harvey.ji123@gmail.com>
2024-12-02 17:56:31 -0600

    Added Skiing to about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,6 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import ATO from "../../assets/img/ATO.jpg";
 import ggGolf from "../../assets/img/ggGolf.jpg";
 import milanDuomo from "../../assets/img/milanDuomo.jpg";
+import breckSki from "../../assets/img/breckSki.jpg";
 import { RiArrowLeftWideFill, RiArrowRightWideFill } from "react-icons/ri";
 
 
@@ -39,6 +40,15 @@ function AboutMe(){
                     <h3>Golf</h3>
                     <p>
                         I'm terrible but pictured here with <a href="https://www.youtube.com/@GoodGood">Good Good Golf</a>
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item  interval={7500}> 
+                <img src={breckSki} className='aboutmePic'/>
+                    <Carousel.Caption>
+                    <h3>Skiing</h3>
+                    <p>
+                       Also trying to learn how to snowboard
                     </p>
                     </Carousel.Caption>
                 </Carousel.Item>
